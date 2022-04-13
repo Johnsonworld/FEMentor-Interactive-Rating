d99fd3a09fa22d1cc81138be1de954e146fd996e
Johnsonworld <mj49926@gmail.com>
2022-04-13 11:51:46 -0600

    Fix borders & apply transitions on buttons. Set up toggle for submit button to display Thank You card

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ function reply_click(value, clicked_id) {
     let selected = clicked_id;
 
 
-    // alert(selected);
-    // document.getElementsById(`button-${selected}`).style.backgroundColor = "red";
 
     document.getElementById("selectedRating").innerHTML = selected;
-    // let btn = document.getElementById(value);
-    // btn.style.backgroundColor = "hsl(217, 12%, 63%)";
-    // btn.style.borderColor = "hsl(217, 12%, 63%)";
-    // btn.style.color = "hsl(0, 0%, 100%)";
     toggleButton(value);
 }
 
 function toggleButton(value) {
+
     let element = document.getElementById(value);
     element.classList.toggle("clickedButton");
+
+
 }
+
+document.getElementById("submit-button").onclick = function () {
+    document.getElementById("main-card").style.display = "none";
+    document.getElementById("state-start").style.display = "unset";
+
+}
+
