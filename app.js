c86538603a1c0a6f47a6cd2d5ff65bbc1434b5db
Johnsonworld <mj49926@gmail.com>
2022-04-15 21:31:44 -0600

    Add radio button type buttons for selector and JS functionality.  Add submission docs.

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-function reply_click(value, clicked_id) {
+function reply_click(btnId, clicked_id) {
+    const btns = document.querySelectorAll('.select-stars');
+    btns.forEach((btn) => {
+        if (btn == btnId) { btn.classList.add("active"); }
+        else { btn.classList.remove("active"); }
+    });
     let selected = clicked_id;
-
-
-
     document.getElementById("selectedRating").innerHTML = selected;
-    toggleButton(value);
+    toggleButton(btnId);
 }
 
 function toggleButton(value) {
 
     let element = document.getElementById(value);
-    element.classList.toggle("clickedButton");
-
-
+    element.classList.toggle("active");
 }
 
 document.getElementById("submit-button").onclick = function () {
@@ -20,4 +20,6 @@ document.getElementById("submit-button").onclick = function () {
     document.getElementById("state-start").style.display = "unset";
 
 }
+
+
 
